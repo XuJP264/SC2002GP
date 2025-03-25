8d9b4553221b50cea8bf89bb6a9b24eb077270a8
XuJP264 <xu0029ng@e.ntu.edu.sg>
2025-03-25 21:23:06 +0800

    the array of the Manager class

--- src/DatabaseInitialization.java
+++
@@ -1,6 +0,0 @@
-public class DatabaseInitialization {
-    public static void main(String[] args) {}
-    public static void initializeDatabase() {
-        // Code to initialize the database
-    }
-}
--- src/Manager.java
+++ src/Manager.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
+
 public class Manager extends UserCard {
     private String identity;
+
     public Manager(String name, int age, String NRIC, String marital_status, String password, String identity) {
         super(name, age, NRIC, marital_status, password);
         this.identity = identity;
     }
+
     public String getIdentity() {
         return identity;
+    }
+}
+
+public class ManagerList {
+    private ArrayList<Manager> managers;
+    public ManagerList() {
+        managers = new ArrayList<>();
+    }
+    public ArrayList<Manager> getManagers() {
+        return managers;
     }
 }
