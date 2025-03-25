80704a2dfbfb75a239907e646ea4219801fc2f2c
XuJP264 <xu0029ng@e.ntu.edu.sg>
2025-03-25 11:40:50 +0800

    重新上传所有文件并更新.gitignore

---
+++ src/DatabaseInitialization.java
@@ -0,0 +1,6 @@
+public class DatabaseInitialization {
+    public static void main(String[] args) {}
+    public static void initializeDatabase() {
+        // Code to initialize the database
+    }
+}
---
+++ src/UserCard.java
@@ -0,0 +1,30 @@
+public class UserCard {
+    private String name;
+    private int age;
+    private String NRIC;
+    private String marital_status;
+    private String password;
+    public UserCard(String name, int age, String NRIC, String marital_status, String password) {
+        this.name = name;
+        this.age = age;
+        this.NRIC = NRIC;
+        this.marital_status = marital_status;
+        this.password = password;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void getNRIC() {
+        return NRIC;
+    }
+    public String getMarital_status() {
+        return marital_status;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
+
